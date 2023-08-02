84841763a1930d0b16929ea0c1c985775ee48cb2
relani <manishrelani4598@gmail.com>
2023-08-03 01:13:01 +0530

    removing unwanted commeted

--- array.js
+++ array.js
@@ -1,14 +1,7 @@
-const data = [12,34,65,34,54,33,23];
+const data = [12, 34, 65, 34, 54, 33, 23];
 console.log("hello ",)
-for (let a=0;a<data.length;a++){
+for (let a = 0; a < data.length; a++) {
     console.log("hello ", data[a])
 }
 // accessing
-console.log("acces",data[8])
-// new chane in somya
-
-// hi these are few changes in want in main
-
-//  but lets see if it goes
-
-// my main js file no new feature
+console.log("acces", data[8])
